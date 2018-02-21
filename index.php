3e032907128188cd3d7b935a5833eeec518877e4
Nina LaMastra <agl24@afsconnect1.njit.edu>
2018-02-21 13:40:09 -0500

    Added search for backslashes in  and printed out all positions (Question 4)

--- index.php
+++ index.php
@@ -6,18 +6,28 @@
 	$year = array("2012", "396", "300","2000", "1100", "1089");
 	echo "The value of \$year: ";
 	print_r($year);
-	echo "<br>"
+	echo "<br>";
 
 	if (($date - $tar) > 0)
 	{
-	  echo "The future";
+	  echo "The future<br>";
 	}
 	else if (($date - $tar) < 0)
 	{
-	  echo "The past";
+	  echo "The past<br>";
 	}
 	else
 	{
-	  echo "oops";
+	  echo "oops<br>";
+	}
+
+
+	echo "Backslashes appear at the following positions in \$date: ";
+	for($stringIndex = 0; $stringIndex < strlen($date); $stringIndex++)
+	{
+		if ($date[$stringIndex] == "/")
+		{
+			echo $stringIndex." ";
+		}
 	}
 ?>
